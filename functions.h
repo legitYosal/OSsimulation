b438661108afee85a71d1786dec9c0e6a291dc03
usefss <flasnm@yahoo.com>
2020-01-29 15:07:41 +0330

    syntax debuging

--- functions.h
+++ functions.h
@@ -2,12 +2,12 @@
 #define FUNCTIONS_HEADER
 
 #include "processStrct.h"
-void swap(char* a, char* b);
-int sortAreadinp(struct Process* processArr);
 void showqueue(struct Process* queue, int limit);
 void showProcessInf(struct Process p);
-void FIFOadd(struct Process* p, struct Process* queue, int* limit);
-void* FIFOextract(int witchone, struct Process* queue, int* limit);
 void showqueueByname(struct Process* queue, int limit);
+void swap(char* a, char* b);
+int sortAreadinp(struct Process* processArr);
+void* FIFOextract(int witchone, struct Process* queue, int* limit);
+void FIFOadd(struct Process* p, struct Process* queue, int* limit);
 
 #endif
